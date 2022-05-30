b6bbcf8425c3b53aa7eddda257ab7bc4b03ed08d
Vicachu42 <69855483+Vicachu42@users.noreply.github.com>
2022-05-30 10:18:03 +0200

    Added basic routing
    Co-authored-by: Sofia Johansson <70485216+Sofianossnahoj@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./sass/App.scss";
-import CreateNewEntry from "./views/CreateNewEntry";
+import LandingPage from "./views/LandingPage";
 import HomeView from "./views/HomeView";
 import SignIn from "./components/SignIn";
+import CreateNewEntry from "./views/CreateNewEntry";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <HomeView />
-      <br />
-      <CreateNewEntry />
-      <br />
-      <SignIn />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/home">Home</Link>
+          <Link to="/signin">Sign In</Link>
+          <Link to="/create">Create New Entry</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <HomeView />
+              </ProtectedRoute>
+              }
+          />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/create" element={<CreateNewEntry />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
