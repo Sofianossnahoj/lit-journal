764234b50a29de3d055a362bb638043b2f44e8f8
Vicachu42 <69855483+Vicachu42@users.noreply.github.com>
2022-06-01 10:56:53 +0200

    Bugfix Landing routing and favicon
    Co-Authored-By: Sofia Johansson <70485216+Sofianossnahoj@users.noreply.github.com>

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,11 +1,21 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
 import SignIn from "../components/SignIn";
-import { Link } from "react-router-dom";
 
 function LandingPage() {
+  const currentUser = useSelector(selectUser);
+
+  if (currentUser !== null) {
+    return <Navigate to="/home" />;
+  } else {
+    console.log('fuck this shit')
+  }
+
   return (
     <section className="landing">
       <p>Welcome to LITerature Journal</p>
-      <Link to="/home"><SignIn /></Link>
+      <SignIn />
     </section>
   );
 }
