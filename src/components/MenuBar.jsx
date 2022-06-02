7f543c877802b48df76f0f9a5fe7b42f26188393
Vicachu42 <69855483+Vicachu42@users.noreply.github.com>
2022-06-02 10:21:22 +0200

    WIP Styling
    Added menu icons and favicon. Started working on the menu bar component.

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import "../sass/components/menuBar.scss";
 import { Link } from "react-router-dom";
 import SignOut from "./SignOut";
 
 function MenuBar() {
   return (
     <nav>
-      <Link to="/home"><button>Home</button></Link>
-      <Link to="/create"><button>Create</button></Link>
-      <Link to="/search"><button>Search</button></Link>
-      <Link to="/"><SignOut /></Link>
+      <Link to="/home">
+        <button className="menu-button">
+          <img src="src/images/house-solid.svg" alt="home" className="menu-icons" />
+          Home
+        </button>
+      </Link>
+      <Link to="/search">
+        <button className="menu-button">
+          <img src="src/images/magnifying-glass-solid.svg" alt="search" className="menu-icons" />
+          Search
+        </button>
+      </Link>
+      <Link to="/">
+        <SignOut />
+      </Link>
     </nav>
   );
 }
