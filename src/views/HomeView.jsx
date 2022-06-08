78ea56b6c046a8ecb9f3fbc609e16be00380d012
Sofianossnahoj <70485216+Sofianossnahoj@users.noreply.github.com>
2022-06-08 15:34:50 +0200

    Styling homeView

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../sass/views/homeView.scss";
+
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { setEntries, getEntries, deleteEntry } from "../features/entriesSlice";
@@ -18,6 +19,7 @@ import MenuBar from "../components/MenuBar";
 function HomeView() {
   const entries = useSelector(getEntries);
   const currentUser = useSelector(selectUser);
+  const [isVisible, setIsVisible] = useState(false);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -71,16 +73,60 @@ function HomeView() {
         <SearchBar />
       </div>
 
-      {entries ? (
+      {entries.length > 0 ? (
         <span>
-          {entries.map((val, id) => {
+          {entries.map((note, id) => {
             // console.log("logs val in template map", val.id);
             return (
-              <div key={id}>
-                <p>{val.title}</p>
-                <p>{val.author}</p>
-                <button onClick={() => handleDelete(val.id)}>Delete</button>
-              </div>
+              <article key={id} className="note-container">
+                <section className="note-collapsed">
+                  <label className="note-label">Title</label>
+                  <p className="note-text">{note.title}</p>
+                  <label className="note-label">Author</label>
+                  <p className="note-text">{note.author}</p>
+                  <label className="note-label">Genre</label>
+                  <p className="note-text">{note.genre}</p>
+                  <label className="note-label">Method</label>
+                  <p className="note-text">{note.method}</p>
+                </section>
+                {isVisible ? (
+                  <section className="">
+                    <label className="note-label">Was it worth the read?</label>
+                    <p className="note-text">{note.worthit}</p>
+                    <label className="note-label">Favorite chapter</label>
+                    <p className="note-text">{note.favoriteChapter}</p>
+                    <label className="note-label">Favorite character</label>
+                    <p className="note-text">{note.favoriteCharacter}</p>
+                    <label className="note-label">
+                      Sequel, will you read it?
+                    </label>
+                    <p className="note-text">{note.sequel}</p>
+                    <label className="note-label">Quotes</label>
+                    <p className="note-text">{note.quotes}</p>
+                  </section>
+                ) : (
+                  <p></p>
+                )}
+                <section className="note-button">
+                  <button
+                    className="note-button-icon"
+                    onClick={() => handleDelete(note.id)}
+                  >
+                    <img
+                      className="icon"
+                      src="src/images/trash-can-grey.png"
+                      alt="Delete"
+                    />
+                  </button>
+                  {!isVisible ? (
+                    <button onClick={() => setIsVisible(true)}>See More</button>
+                  ) : (
+                    <button onClick={() => setIsVisible(false)}>
+                      See Less
+                    </button>
+                  )}
+                </section>
+              </article>
             );
           })}
         </span>
@@ -101,7 +147,6 @@ function HomeView() {
           <button>Create New Entry</button>
         </Link>
       </section> */}
-
       <MenuBar />
     </main>
   );
